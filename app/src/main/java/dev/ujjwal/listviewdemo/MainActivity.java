e552563d7e99fa5165d6b20ba72396d826c7580c
ujjwal <ujjwalmaity98@gmail.com>
2019-11-06 14:46:05 +0530

    Make ListView clickable

--- app/src/main/java/dev/ujjwal/listviewdemo/MainActivity.java
+++ app/src/main/java/dev/ujjwal/listviewdemo/MainActivity.java
@@ -3,10 +3,14 @@ package dev.ujjwal.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     ListView listView;
 
@@ -19,5 +23,12 @@ public class MainActivity extends AppCompatActivity {
         String[] languages = {"JavaScript", "Python", "Java", "C", "C++", "PHP", "Swift", "C#", "Ruby", "Objective-C", "SQL", "Go", "Perl", "Scala", "Apex", "R", "SAS"};
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, languages);
         listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        TextView textView = (TextView) view;
+        Toast.makeText(this, textView.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
