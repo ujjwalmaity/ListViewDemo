c1622f58348752dbc64e0c8750577405ce9dda57
ujjwal <ujjwalmaity98@gmail.com>
2019-11-06 14:43:27 +0530

    Implement ListView

--- app/src/main/java/dev/ujjwal/listviewdemo/MainActivity.java
+++ app/src/main/java/dev/ujjwal/listviewdemo/MainActivity.java
@@ -3,12 +3,21 @@ package dev.ujjwal.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list_view);
+        String[] languages = {"JavaScript", "Python", "Java", "C", "C++", "PHP", "Swift", "C#", "Ruby", "Objective-C", "SQL", "Go", "Perl", "Scala", "Apex", "R", "SAS"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, languages);
+        listView.setAdapter(arrayAdapter);
     }
 }
